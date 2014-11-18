7ba7e6eda5165980670969737328ebdd84eeba67
willcai1984 <willcai1984@gmail.com>
2014-11-18 17:24:00 +0800

    Modify example

--- scripts/example/__init__.py
+++ scripts/example/__init__.py
@@ -13,6 +13,6 @@ class example(object):
         self.w.driver.get(url)
         self.w.wait_until_element_displayed(baidu_keyword_input)
         self.w.info('Get url successfully', True)
-        self.w.input(baidu_keyword_input,keyword)
+        self.w.input(baidu_keyword_input, keyword)
         self.w.click(baidu_search_btn)
         self.w.info('Search keyword successfully', True)
